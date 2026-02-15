15ac8e0b340b4eaba0075761b2dde87b2d9158af
V SS Karthik <vsskartheek@gmail.com>
2026-02-15 19:12:32 +0530

    This file is to replace hardcoded API's

---
+++ frontend/src/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    // Dev Mode : LocalHost
+    API_BASE_URL : "http://127.0.0.1:5000/api"
+
+    // Production Mode:
+    //API_BASE_URL: "http://192.168.1.100:5000/api",
+
+};
+
+export default CONFIG;
